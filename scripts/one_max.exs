6ab5c8e51a9a85db483247d3301fcb47f73371b7
EL <llenriquelopez@gmail.com>
2021-06-30 22:12:45 -0500

    as author

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,6 +1,8 @@
-genotype = fn -> for _ <- 1,,1000, do: Enum.random(1,0) end
-fitness_function = fn chromosome -> Enum.sum(chromosome) end
+genotype = fn -> for _ <- 1,,1000, do: Enum.random(0..1) end
+
 max_fitness = 1000
+
+fitness_function = fn chromosome -> Enum.sum(chromosome) end
 
 soln = Genetic.run(fitness_function, genotype, max_fitness)
 
