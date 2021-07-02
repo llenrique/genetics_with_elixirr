360cf624d2919064a2b790c0fa2f16ac3702f81d
EL <llenriquelopez@gmail.com>
2021-07-02 12:24:19 -0500

    speller

--- scripts/speller.exs
+++ scripts/speller.exs
@@ -6,16 +6,16 @@ defmodule Speller do
     genes =
       Stream.repeatedly(fn -> Enum.random(?a..?z) end)
       |> Enum.take(34)
-      %Chromosome{genes: genes, size: 34}
+    %Chromosome{genes: genes, size: 34}
   end
 
   def fitness_function(chromosome) do
     target = "supercalifragilisticexpialidocious"
     guess = chromosome.genes
-    String.jaro_distance(target, List.to_string(guess))
+    String.bag_distance(target, List.to_string(guess))
   end
 
-  def terminate?([best | _] = _population), do: best.fitness == 1
+  def terminate?(population), do: hd(population).fitness == 1
 end
 
 soln = Genetic.run(Speller)
