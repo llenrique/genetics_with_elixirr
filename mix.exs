3ce34fcb937bfc4410d834edc911bef7bc7bf544
EL <llenriquelopez@gmail.com>
2021-07-02 12:23:07 -0500

    removes warnings as errors

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Genetic.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_options: [warnings_as_errors: false]
     ]
   end
 
