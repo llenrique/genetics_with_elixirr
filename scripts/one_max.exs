b05354afb3d8ae2be26a3856b50dd1077c42b385
EL <llenriquelopez@gmail.com>
2021-07-02 12:24:11 -0500

    one max module

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,10 +1,24 @@
-genotype = fn -> for _ <- 1..1000, do: Enum.random(0..1) end
+defmodule OneMax do
+  @behaviour Problem
 
-max_fitness = 1000
+  alias Types.Chromosome
 
-fitness_function = fn chromosome -> Enum.sum(chromosome) end
+  @impl true
+  def genotype do
+    genes = for _ <- 1..42, do: Enum.random(0..1)
 
-soln = Genetic.run(fitness_function, genotype, max_fitness)
+    %Chromosome{genes: genes, size: 42}
+  end
 
-IO.write("\m")
-IO.inspect soln
+  @impl true
+  def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
+
+  @impl true
+  def terminate?([best | _]), do: best.fitness == 42
+end
+
+
+soln = Genetic.run(OneMax)
+
+IO.write("\n")
+IO.inspect(soln, limit: :infinity)
