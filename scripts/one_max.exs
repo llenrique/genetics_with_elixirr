18de6ff57d220803e8d34e184cd96c3070f23572
EL <llenriquelopez@gmail.com>
2021-07-03 19:34:50 -0500

    temperature tracking at end

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -5,20 +5,21 @@ defmodule OneMax do
 
   @impl true
   def genotype do
-    genes = for _ <- 1..42, do: Enum.random(0..1)
+    genes = for _ <- 1..1000, do: Enum.random(0..1)
 
-    %Chromosome{genes: genes, size: 42}
+    %Chromosome{genes: genes, size: 1000}
   end
 
   @impl true
   def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
 
+  # Requires a modification in the Problem behaviour to set the temperature
   @impl true
-  def terminate?([best | _]), do: best.fitness == 42
+  def terminate?([best | _], generation, temperature), do: temperature < 2
 end
 
 
-soln = Genetic.run(OneMax)
+soln = Genetic.run(OneMax, population_size: 1000)
 
 IO.write("\n")
 IO.inspect(soln, limit: :infinity)
